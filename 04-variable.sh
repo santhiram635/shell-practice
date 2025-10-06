e2dbd467556b2098923b488e61264552a106ed26
prasanthi <santhi@ram.com>
2025-10-06 19:48:39 +0530

    ongole

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: welcom to ongole $PERSON2"
+echo "$PERSON2:: finally we both met here"
+echo "$PERSON1:: we have a get to gather"
+echo "$PERSON2:: yes in you collage shdc"
