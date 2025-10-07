3629c1ec9a2cbfeec80b21a4b6e3147ae565ff7f
prasanthi <santhi@ram.com>
2025-10-07 20:33:54 +0530

    given number

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
  echo "given number $NUMBER is less than 10"
+elif [ $NUMBER -eq 10 ]; then
+ echo "given number $NUMBER is equal to 10" 
 else
  echo "given number $NUMBER is greter than 10"
 fi 
