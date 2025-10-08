4c6fbe390640efa00d99359b951beef4162b775b
prasanthi <santhi@ram.com>
2025-10-08 19:17:01 +0530

    installation

---
+++ 13-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+UDERID=$(id -u)
+
+if [ $USERID -ne o ]; then
+  echo "error:please run this script with root user"
+  exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo "error:installing $2 is failure"
+    else
+       echo "installing $2 is success"
+    fi       
+}
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
