296093e1b62327152b54886e6f12274f85766181
prasanthi <santhi@ram.com>
2025-10-10 13:42:43 +0530

    loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -27,7 +27,18 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
+# $@
+
 for package in $@
 do
-    echo "package:$package"
-done    
+    # check package is already installed or not
+    dnf list installed $package &>>$LOG_FILE
+
+    # if exit status is 0, already installed. -ne 0 need to install it
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
+    fi
+done   
